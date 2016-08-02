2026f04054dd9f68fc617da7d5b146e0b4aa509e
Zach H <zdhadgraft@gtcc.edu>
2016-08-02 16:27:24 -0400

    WIP adding in Google Places API Worker.

--- app/services/google_places_api_wrapper.rb
+++ app/services/google_places_api_wrapper.rb
@@ -4,7 +4,7 @@ class GooglePlacesAPIWrapper
 
   include Enumerable
 
-  attr_reader :results
+  attr_reader :results, :raw_results
 
   def initialize raw_results
     @raw_results    = raw_results
@@ -14,8 +14,7 @@ class GooglePlacesAPIWrapper
   def self.for ops={}
     location    = ops[:location]
     term        = ops[:term]
-    raw_results = GooglePlacesAPI.search({location: location, term: term})
-    return self.new(raw_results)
+    GooglePlacesAPI.search({location: location, term: term})
   end
 
   def each &block
@@ -32,7 +31,8 @@ class GooglePlacesAPIWrapper
 
 private
 
-  def process_result r
+def process_result r
+  binding.pry
     sleep( rand(0..64)/1024.0 )
     yr = YelpGemWrapper.find_business(location: r["formatted_address"], term: r["name"])
     return unless yr
